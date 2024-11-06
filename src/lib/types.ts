fae15ec0130f9f3bbc6cf77a4a18684a1034bae5
Rupeshkhati <ykhati907@gmail.com>
2024-11-07 02:37:14 +0400

    33%done

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,6 @@
 import { Notification, Prisma, Role } from '@prisma/client'
 import { getAuthUserDetails, getUserPermissions } from './queires'
+import { db } from './db'
 
 export type NotificationWithUser =
   | ({
@@ -20,6 +21,24 @@ export type NotificationWithUser =
   typeof getUserPermissions
 >
 
+const __getUsersWithAgencySubAccountPermissionsSidebarOptions = async (
+  agencyId: string
+) => {
+  return await db.user.findFirst({
+    where: { Agency: { id: agencyId } },
+    include: {
+      Agency: { include: { SubAccount: true } },
+      Permissions: { include: { SubAccount: true } },
+    },
+  })
+}
+
+
 export type AuthUserWithAgencySigebarOptionsSubAccounts =
   Prisma.PromiseReturnType<typeof getAuthUserDetails>
 
+
+  export type UsersWithAgencySubAccountPermissionsSidebarOptions =
+  Prisma.PromiseReturnType<
+    typeof __getUsersWithAgencySubAccountPermissionsSidebarOptions
+  >
